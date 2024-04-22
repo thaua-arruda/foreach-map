db4b4abe3ba1412e54774180cff88e7baa521be1
thaua-arruda <thauafarruda@gmail.com>
2024-04-22 16:31:04 -0400

    adicionado questao 17

---
+++ questao17.js
@@ -0,0 +1,6 @@
+/*
+Questão 17 - Qual é a diferença entre map() e forEach() em termos de 
+manipulação do array original? 
+*/
+
+// O map() irá criar outrop array mantendo o tamanho do array original e o forEach() irá modificar os elementos do array.
