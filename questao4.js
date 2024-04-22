4ca2e2fb01dc9bbad52fc657adeb82b9f7eccb24
thaua-arruda <thauafarruda@gmail.com>
2024-04-22 16:29:58 -0400

    adicionado questao 4

---
+++ questao4.js
@@ -0,0 +1,11 @@
+/*
+Questão 04 – Crie um algoritmo usando o método forEach() para encontrar a 
+soma de todos os números em um array?
+*/
+
+var numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var total = 0;
+
+function somar(item) { total += item; }
+numeros.forEach(somar);
+console.log(total);
