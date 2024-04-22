e8c5f385cf86286bbc12375231ecf80833cac3f1
thaua-arruda <thauafarruda@gmail.com>
2024-04-22 16:30:02 -0400

    adicionado questao 5

---
+++ questao5.js
@@ -0,0 +1,14 @@
+/*
+Questão 05 – Crie um algoritmo usando o método map() para criar um novo 
+array com o dobro de cada número ímpar em um array original?
+*/
+
+let array = [1, 2, 3, 4, 5, 6];
+let impar = array.map (function(num){
+    if (num % 2 == 1){
+        return num * 2
+    } 
+})
+console.log(impar);
+
+
