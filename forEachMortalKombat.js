89a63c8263d3dd090a7ae01f7efd026c2edfc2ac
thaua-arruda <thauafarruda@gmail.com>
2024-04-22 16:27:29 -0400

    adicionado exemplo de forEachMortalKombat

---
+++ forEachMortalKombat.js
@@ -0,0 +1,4 @@
+const mortalKombat = ["Liu Kang", "Sub-Zero", "Raiden", "Scorpion", "Johnny Cage"];
+mortalKombat.forEach(function(elemento, index, array) {
+    console.log("Index:" + index, "Elemento:" + elemento, "Array:" + array);
+});
